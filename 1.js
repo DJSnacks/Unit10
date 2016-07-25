690b9ed6e4abc255914acec12766b036ed7711ae
DJSnacks <ricardo.carrillo.266@gmail.com>
2016-07-24 23:29:43 -0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,30 @@
+function Country(continent, gdp, population, name) {
+  this.continent = continent;
+  this.gdp = gdp;
+  this.population = population;
+  this.name = name;
+}
+function Restaurant(manager, address, number, owner) {
+  this.manager = manager;
+  this.address = address;
+  this.number = number;
+  this.owner = owner;
+}
+function Album(artist, genre, year, title) {
+  this.artist = artist;
+  this.genre = genre;
+  this.year = year;
+  this.title = title;
+}
+function Team(name, mascot, city, color) {
+  this.name = name;
+  this.mascot = mascot;
+  this.city = city;
+  this.color = color;
+}
+function Movie(title, director, year, producer) {
+  this.title = title;
+  this.director = director;
+  this.year = year;
+  this.producer = producer;
+}
